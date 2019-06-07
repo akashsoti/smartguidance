699fab48a0f49956474cd6d480ba756d4e79bc0b
Akash <akashsoti@gmail.com>
2019-06-07 21:24:40 +0530

    testing

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 //This is an example code to Add Search Bar Filter on Listview//
-import React, { Component } from 'react';
 //import react in our code.
+import React, { Component } from "react";
+import SmartGuidance from "./src/components/SmartGuidanceWidget";
+import PlaceDetail from "./src/components/PlaceDetail";
  
 import {
   Text,
@@ -13,146 +15,56 @@ import {
 } from 'react-native';
 //import all the components we are going to use.
 
-const ab = [
-  {
-    "userId": 1,
-    "id": 1,
-    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-  },
-  {
-    "userId": 1,
-    "id": 2,
-    "title": "qui est esse",
-    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-  }
-]
+
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    //setting default state
-    this.state = { isLoading: false, text: '', display: "none"};
-    this.arrayholder = ab;
-  }
- 
-  // componentDidMount() {
-  //   return fetch('https://jsonplaceholder.typicode.com/posts')
-  //     .then(response => response.json())
-  //     .then(responseJson => {
-  //       this.setState(
-  //         {
-  //           isLoading: false,
-  //           dataSource: responseJson
-  //         },
-  //         function() 
-  //           this.arrayholder = responseJson;
-  //         }
-  //       );
-  //     })
-  //     .catch(error => {
-  //       console.error(error);
-  //     });
-  // }
-  SearchFilterFunction(text) {
-    //passing the inserted text in textinput
-    const newData = this.arrayholder.filter(function(item) {
-      //applying filter for the inserted text in search bar
-      const itemData = item.title ? item.title.toUpperCase() : ''.toUpperCase();
-      const textData = text.toUpperCase();
-      return itemData.indexOf(textData) > -1;
-    });
-    this.setState({
-      //setting the filtered newData on datasource
-      //After setting the data it will automatically re-render the view
-      dataSource: newData,
-      text: text,
-      display: "block",
-    });
-
-    if (text === "") {
-      this.setState({
-        display: "none"
-      })
-    }
-
-  }
-  ListViewItemSeparator = () => {
-    //Item sparator view
-    return (
-      <View
-        style={{
-          height: 0.3,
-          width: '90%',
-          backgroundColor: '#080808',
-        }}
-      />
-    );
+  state = {
+		places: [],
+		selectedPlace: null
   };
+
+  placeAddedHandler = placeName => {
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat({key: Math.random(), name: placeName, image: {uri: 'https://cdn.thecrazytourist.com/wp-content/uploads/2017/09/ccimage-shutterstock_397624165.jpg'}})
+      };
+    });
+  };
+
+	placeDeletedHandler = () => {
+		this.setState(prevState => {
+			return {
+				places: prevState.places.filter(place => {
+					return place.key !== prevState.selectedPlace.key;
+				}),
+				selectedPlace: null
+			};
+		});
+	}
+
+	modalClosedHandler = () => {
+		this.setState({
+			selectedPlace: null
+		});
+	}
+
+	placeSeletedHandler = (key) => {
+		this.setState(prevState =>{
+			return {
+				selectedPlace: prevState.places.find(place => {
+					return place.key === key;
+				})
+			};
+		});
+	}
+
   render() {
-    if (this.state.isLoading) {
-      //Loading View while data is loading
-      return (
-        <View style={{ flex: 1, paddingTop: 20 }}>
-          <ActivityIndicator />
-        </View>
-      );
-    }
-
-    const listDisplay = this.state.display;
-
     return (
-      //ListView to show with textinput used as search bar
-      <View style={styles.viewStyle}>
-        <Text style={styles.searchWidgetTitle}>Amazon Assistance</Text>
-        <TextInput
-          style={styles.textInputStyle}
-          onChangeText={text => this.SearchFilterFunction(text)}
-          value={this.state.text}
-          underlineColorAndroid="transparent"
-          placeholder="Get answers related to this product"
-        />
-        <View style={styles.searchListResults}>
-          <FlatList
-            data={this.state.dataSource}
-            ItemSeparatorComponent={this.ListViewItemSeparator}
-            renderItem={({ item }) => (
-              <Text style={styles.textStyle}>{item.title}</Text>
-            )}
-            enableEmptySections={true}
-            style={{ display: listDisplay, height : 500, borderColor: "black" }}
-            keyExtractor={(item, index) => index}
-          />
-        </View>
-      </View>
+      <PlaceDetail selectedPlace={this.state.selectedPlace} onItemDeleted={this.placeDeletedHandler} onModalClosed={this.modalClosedHandler}/>
+      <SmartGuidance />
     );
   }
 }
+
 const styles = StyleSheet.create({
-  searchListResults: {
-    borderWidth: 1,
-    borderColor: "red",
-  },
-  searchWidgetTitle: {
-    fontWeight: "bold",
-    fontSize: 18,
-    marginTop: 10,
-    marginBottom: 10,
-    borderColor: "#e59D52",
-  },
-  viewStyle: {
-    justifyContent: 'center',
-    marginTop: 40,
-    padding: 16,
-  },
-  textStyle: {
-    padding: 10,
-  },
-  textInputStyle: {
-    height: 40,
-    borderWidth: 1,
-    paddingLeft: 10,
-    borderColor: '#009688',
-    backgroundColor: '#FFFFFF',
-  },
 });
---
+++ src/components/SmartGuidanceWidget.js
@@ -0,0 +1,150 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, Modal, Image, Button, TextInput, FlatList } from 'react-native';
+
+const ab = [
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "qui est esse",
+    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+  }
+]
+
+class SmartGuidance extends Component {
+  constructor(props) {
+    super(props);
+    //setting default state
+    this.state = { isLoading: false, text: '', display: "none", borderWidth: 0};
+    this.arrayholder = ab;
+  }
+ 
+  // componentDidMount() {
+  //   return fetch('https://jsonplaceholder.typicode.com/posts')
+  //     .then(response => response.json())
+  //     .then(responseJson => {
+  //       this.setState(
+  //         {
+  //           isLoading: false,
+  //           dataSource: responseJson
+  //         },
+  //         function() 
+  //           this.arrayholder = responseJson;
+  //         }
+  //       );
+  //     })
+  //     .catch(error => {
+  //       console.error(error);
+  //     });
+  // }
+  SearchFilterFunction(text) {
+    //passing the inserted text in textinput
+    const newData = this.arrayholder.filter(function(item) {
+      //applying filter for the inserted text in search bar
+      const itemData = item.title ? item.title.toUpperCase() : ''.toUpperCase();
+      const textData = text.toUpperCase();
+      return itemData.indexOf(textData) > -1;
+    });
+    this.setState({
+      //setting the filtered newData on datasource
+      //After setting the data it will automatically re-render the view
+      dataSource: newData,
+      text: text,
+      display: "block",
+    });
+
+    if (text === "") {
+      this.setState({
+        display: "none",
+        borderWidth: 0,
+      });
+    }
+
+  }
+  ListViewItemSeparator = () => {
+    //Item sparator view
+    return (
+      <View
+        style={{
+          height: 0.3,
+          width: '90%',
+          backgroundColor: '#080808',
+        }}
+      />
+    );
+  };
+  render() {
+    if (this.state.isLoading) {
+      //Loading View while data is loading
+      return (
+        <View style={{ flex: 1, paddingTop: 20 }}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+
+    const listDisplay = this.state.display;
+
+    return (
+      //ListView to show with textinput used as search bar
+      <View style={styles.viewStyle}>
+        <Text style={styles.searchWidgetTitle}>Amazon Assistance</Text>
+        <TextInput
+          style={styles.textInputStyle}
+          onChangeText={text => this.SearchFilterFunction(text)}
+          value={this.state.text}
+          underlineColorAndroid="transparent"
+          placeholder="Get answers related to this product"
+        />
+        <View style={styles.searchListResults}>
+          <FlatList
+            data={this.state.dataSource}
+            ItemSeparatorComponent={this.ListViewItemSeparator}
+            renderItem={({ item }) => (
+              <Text onPress={(item) => alert('item clicked ' + item.id)} style={styles.textStyle}>{item.title}</Text>
+            )}
+            enableEmptySections={true}
+            style={{ display: listDisplay, height : 200, borderColor: "black" }}
+            keyExtractor={(item, index) => index}
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  searchListResults: {
+    borderWidth: 1,
+    borderColor: "red",
+  },
+  searchWidgetTitle: {
+    fontWeight: "bold",
+    fontSize: 18,
+    marginTop: 10,
+    marginBottom: 10,
+    borderColor: "#e59D52",
+  },
+  viewStyle: {
+    justifyContent: 'center',
+    marginTop: 40,
+    padding: 16,
+  },
+  textStyle: {
+    padding: 10,
+  },
+  textInputStyle: {
+    height: 40,
+    borderWidth: 1,
+    paddingLeft: 10,
+    borderColor: '#009688',
+    backgroundColor: '#FFFFFF',
+  },
+});
+
+export default SmartGuidance;
